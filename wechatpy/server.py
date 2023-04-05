77c23b6ce3190acaedbaffc152f93f6888cb353d
xsm <xingshimeng@fancydigital.com.cn>
2023-04-06 07:58:33 +0800

    a

--- wechatpy/server.py
+++ wechatpy/server.py
@@ -4,7 +4,9 @@ from flask import Flask, request
 from wechatpy import parse_message
 from gevent import pywsgi
 import json
+from wechatpy import WeChatClient
 
+client = WeChatClient('appid', 'secret')
 app = Flask(__name__) #实例化Flask对象app
 
 @app.route('/wechat_msg', methods=['GET', 'POST']) #app中的route装饰器
@@ -14,11 +16,15 @@ def hello_world():
     signature = request.args['signature']
     timestamp = request.args['timestamp']
     nonce = request.args['nonce']
+    openid = request.args['openid']
     raw_data = request.data
-    print("xxx2", signature, timestamp, nonce, raw_data)
- 
+    print("xxx2", signature, timestamp, nonce, openid, raw_data)
+
+    res = client.message.send_text(openid, '222')
+    print("xxx3", res)
+
     msg = parse_message(raw_data)
-    print("xxx3", msg)
+    print("xxx4", msg)
     
     # try:
     #     check_signature(token, signature, timestamp, nonce)
@@ -33,3 +39,7 @@ if __name__ == '__main__':
     print("server start...")
     server.serve_forever()
     
+
+def read_file(path):
+    with open(path, mode='r', encoding='utf-8') as f:
+        return f.read()
