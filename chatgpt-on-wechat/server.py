15b4055259d2ec1657bb8ef2bb7e7d91a3242887
xsm <xingshimeng@fancydigital.com.cn>
2023-04-07 20:50:06 +0800

    a

--- chatgpt-on-wechat/server.py
+++ chatgpt-on-wechat/server.py
@@ -11,9 +11,12 @@ app = Flask(__name__) #实例化Flask对象app
 
 @app.route('/', methods=['GET', 'POST']) #app中的route装饰器
 def hello_world():
-    session = request.json['session']
-    query = request.json['query']
-    system = request.json['system']
+    print(request.args)
+    print(request.data)
+    print(request)
+    # session = request.json['session']
+    # query = request.json['query']
+    # system = request.json['system']
 
     # session = request.args['session']
     # query = request.args['query']
