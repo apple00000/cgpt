82b5615a2cd288f018d9a5fd970f83d3f76d6488
xsm <xingshimeng@fancydigital.com.cn>
2023-04-08 17:52:05 +0800

    a

--- wechatpy/test.py
+++ wechatpy/test.py
@@ -1,10 +1,14 @@
-from elasticsearch import Elasticsearch
+import es
 
-# 默认host为localhost,port为9200.但也可以指定host与port
-es = Elasticsearch("http://0.0.0.0:9200")
+es.get_es_all_data()
+
+# from elasticsearch import Elasticsearch
+
+# # 默认host为localhost,port为9200.但也可以指定host与port
+# es = Elasticsearch("http://0.0.0.0:9200")
  
-# 插入数据,index，doc_type名称可以自定义，id可以根据需求赋值,body为内容
-es.index(index="index", document={"title":"python2", "content":"上海"})
+# # 插入数据,index，doc_type名称可以自定义，id可以根据需求赋值,body为内容
+# es.index(index="index", document={"title":"python2", "content":"上海"})
 
 # query = {'query': {'match_all': {}}}# 查找所有文档
 # query_name = {'query': {'match': {'content':'深' }}}# 查找所有文档
