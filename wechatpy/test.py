8aff78307bae3d25b4631f8844292196f2672561
xsm <xingshimeng@fancydigital.com.cn>
2023-04-08 17:34:52 +0800

    a

--- wechatpy/test.py
+++ wechatpy/test.py
@@ -7,6 +7,13 @@ es = Elasticsearch("http://0.0.0.0:9200")
 # es.index(index="index", document={"title":"python", "content":"深圳"})
 
 query = {'query': {'match_all': {}}}# 查找所有文档
+query_name = {'query': {'match': {'name':'深' }}}# 查找所有文档
 
-result = es.search(index="index", body=query)
+result = es.search(index="index", body=query_name)
 print('result', result)# 返回第一个文档的内容
+
+
+query_name_2 = {'query': {'match': {'name':'上' }}}# 查找所有文档
+
+result2 = es.search(index="index", body=query_name_2)
+print('result2', result2)# 返回第一个文档的内容
