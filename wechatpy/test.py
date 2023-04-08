1f04d23037011096852e387068e8dec3b279feec
xsm <xingshimeng@fancydigital.com.cn>
2023-04-08 17:29:59 +0800

    a

--- wechatpy/test.py
+++ wechatpy/test.py
@@ -4,4 +4,9 @@ from elasticsearch import Elasticsearch
 es = Elasticsearch("http://0.0.0.0:9200")
  
 # 插入数据,index，doc_type名称可以自定义，id可以根据需求赋值,body为内容
-es.index(index="index", document={"title":"python", "content":"深圳"})
+# es.index(index="index", document={"title":"python", "content":"深圳"})
+
+query = {'query': {'match_all': {}}}# 查找所有文档
+
+result = es.search(index="index", body=query)
+print('result', result)# 返回第一个文档的内容
